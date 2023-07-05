257d7a81399a9746d483b93a9901973895ef06c0
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-05 12:34:24 +0900

    init

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: resolve(__dirname, "src"),
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, './src/index.html'),
+        lesson01: resolve(__dirname, './src/task01/index.html'),
+      },
+    },
+  },
+})
