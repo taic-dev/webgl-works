8cc2187631dec3639d6899060402addf734aa8ac
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-05 18:59:01 +0900

    add: create workflows

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
+  base: "/webgl-shool-task/",
   pages: resolve(__dirname, 'src', 'pages'),
   build: {
     rollupOptions: {
       input: {
         index: resolve(__dirname, './src/index.html'),
-        lesson01: resolve(__dirname, './src/task01/index.html'),
+        lesson01: resolve(__dirname, './src/pages/task01/index.html'),
       },
     },
   },
