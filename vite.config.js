006681af77eec69a0e7b952e0613c2cc9126859e
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-06 23:48:29 +0900

    fix: change vite config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: "src",
   base: '/webgl-school-task/',
   build: {
     outDir: './dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, 'index.html'),
+        index: resolve(__dirname, 'src/index.html'),
         task01: resolve(__dirname, 'src/task01/index.html'),
       },
     },
