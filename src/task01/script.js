7d3dcbb96a48c4e3658fd627b026081b8b172cbe
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-06 12:46:22 +0900

    fix: import path

--- src/task01/script.js
+++ src/task01/script.js
@@ -1,6 +1,6 @@
-import * as THREE from "../../node_modules/three/build/three.module.js"
-import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls';
-import GUI from "../../node_modules/lil-gui";
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GUI from "lil-gui";
 
 window.addEventListener('DOMContentLoaded', () => {
   const app = new App3();
