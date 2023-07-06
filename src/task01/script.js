8aac8ce61b9b682d61b125b60559ee07e834dca9
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-06 13:30:59 +0900

    fix: top page

--- src/task01/script.js
+++ src/task01/script.js
@@ -1,6 +1,6 @@
-import * as THREE from "../../node_modules/three/build/three.module.js";
-import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import GUI from "../../node_modules/lil-gui/dist/lil-gui.esm.js";
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import GUI from "lil-gui";
 
 window.addEventListener('DOMContentLoaded', () => {
   const app = new App3();
