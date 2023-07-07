d8843e2a4f92ac03307ff2ed7ae22fee18af6920
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-07-07 21:50:23 +0900

    add: create practice01

---
+++ src/practice01/script.js
@@ -0,0 +1,143 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import GUI from "lil-gui";
+
+window.addEventListener("DOMContentLoaded",()=>{
+  const app = new App3;
+  app.init();
+  app.render();
+});
+
+
+class App3 {
+  static get CAMERA_PARAM() {
+    return {
+      fovy: 60,
+      aspect: window.innerWidth / window.innerHeight,
+      near: 1,
+      far: 100.0,
+      x: 0.0,
+      y: 0.0,
+      z: 4.0,
+      lookAt: new THREE.Vector3(0.0, 0.0, 0.0),
+    }
+  }
+
+  static get RENDERER_PARAM() {
+    return {
+      clearColor: 0x000000,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  static get DIRECTIONAL_LIGHT_PARAM() {
+    return {
+      clearColor: 0xffffff,
+      intensity: 1.0,
+      x: 0,
+      y: 1.0,
+      z: 1.0,
+    }
+  }
+
+  static get AMBIENT_LIGHT_PARAM() {
+    return {
+      color: 0xffffff,
+      intensity: 0.8,
+    }
+  }
+
+  static get MATERIAL_PARAM() {
+    return {
+      map: new THREE.TextureLoader().load("./img/card.png"),
+    }
+  }
+
+  constructor() {
+    this.renderer;
+    this.scene;
+    this.camera;
+    this.directionalLight;
+    this.ambientLight;
+    this.material;
+    this.card;
+    this.controls;
+    this.boxGeometry;
+    this.axesHelper
+    this.render = this.render.bind(this);
+  }
+
+  init() {
+    this.renderer = new THREE.WebGLRenderer({alpha: true, antialias: true,});
+    this.renderer.setClearColor(new THREE.Color(App3.RENDERER_PARAM.clearColor), 0);
+    this.renderer.setSize(App3.RENDERER_PARAM.width, App3.RENDERER_PARAM.height);
+    const wrapper = document.querySelector("#webgl");
+    wrapper.appendChild(this.renderer.domElement);
+
+    this.scene = new THREE.Scene();
+
+    this.camera = new THREE.PerspectiveCamera(
+      App3.CAMERA_PARAM.fovy,
+      App3.CAMERA_PARAM.aspect,
+      App3.CAMERA_PARAM.near,
+      App3.CAMERA_PARAM.far,
+    )
+    this.camera.position.set(
+      App3.CAMERA_PARAM.x,
+      App3.CAMERA_PARAM.y,
+      App3.CAMERA_PARAM.z,
+    )
+    this.camera.lookAt(App3.CAMERA_PARAM.lookAt);
+
+    this.directionalLight = new THREE.DirectionalLight(
+      App3.DIRECTIONAL_LIGHT_PARAM.color,
+      App3.DIRECTIONAL_LIGHT_PARAM.intensity,
+    )
+    this.directionalLight.position.set(
+      App3.DIRECTIONAL_LIGHT_PARAM.x,
+      App3.DIRECTIONAL_LIGHT_PARAM.y,
+      App3.DIRECTIONAL_LIGHT_PARAM.z,
+    )
+    this.scene.add(this.directionalLight);
+
+    this.ambientLight = new THREE.AmbientLight(
+      App3.AMBIENT_LIGHT_PARAM.color,
+      App3.AMBIENT_LIGHT_PARAM.intensity,
+    )
+    this.scene.add(this.directionalLight);
+
+    this.material = new THREE.MeshPhongMaterial(App3.MATERIAL_PARAM);
+    this.material.shininess = 100;
+    this.boxGeometry = new THREE.BoxGeometry(1.2, 2.0, 0.02);
+    this.card = new THREE.Mesh(this.boxGeometry, this.material);
+    this.card.rotation.z = -0.5;
+    this.scene.add(this.card);
+    
+    const axesBarLength = 5.0;
+    this.axesHelper = new THREE.AxesHelper(axesBarLength);
+    this.scene.add(this.axesHelper);
+
+    // コントロール
+    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+  }
+
+  render() {
+    requestAnimationFrame(this.render);
+    
+    // window.addEventListener("mousemove", (e) => {
+    //   this.card.rotation.x = e.clientX - this.card.position.x * 0.003
+    //   this.card.rotation.y = e.clientY - this.card.position.x * 0.003
+    // })
+
+    window.addEventListener("scroll", () => {
+      // console.log(this.card.position.y, window.pageYOffset);
+      this.card.position.y = window.scrollY * 0.005;
+      this.card.rotation.x = -window.scrollY * 0.005;
+    })
+    this.card.rotation.y += 0.005;
+
+
+    this.renderer.render(this.scene ,this.camera);
+  }
+}
