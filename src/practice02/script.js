b0b2209ef80b0b59c4d7f4ed8e84d638c663ae66
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-08-30 00:38:39 +0900

    practice add hand mesh

--- src/practice02/script.js
+++ src/practice02/script.js
@@ -1,5 +1,7 @@
 import "./style.css";
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import handGltfData from './img/hand.gltf'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -13,29 +15,35 @@ window.addEventListener("DOMContentLoaded", () => {
   // シーンの作成
   const scene = new THREE.Scene();
 
-  // 眼球
-  const eyeGeometry = new THREE.SphereGeometry(5, 64, 32);
-  const eyeMaterial = new THREE.MeshLambertMaterial({
-    color: 0x8dc3ff,
-  });
-  const eyeMesh = new THREE.Mesh(eyeGeometry, eyeMaterial);
-  eyeMesh.position.set(0, 0, 0);
-  scene.add(eyeMesh);
+  // 手
+  let handGltf
+  const gltfLoader = new GLTFLoader();
+  gltfLoader.load(handGltfData, (gltf) => {
+    handGltf = gltf.scene
+    handGltf.position.set(0, 0, 0)
+    handGltf.rotation.set(0, -4.5, -5)
+    scene.add(handGltf)
+  })
 
   // カメラ
   const camera = new THREE.PerspectiveCamera(
-    45,
+    5,
     window.innerWidth / window.innerHeight,
     1,
     1000
   );
-  camera.position.set(15, 15, 15)
+  camera.position.set(15, 10, 15)
   camera.lookAt(new THREE.Vector3(0.0, 0.0, 0,0));
   scene.add(camera);
+  
+  // カメラ操作
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.2;
 
   // 平行光源
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
-  directionalLight.position.set(1, 5, 5);
+  directionalLight.position.set(1, 10, 5);
   scene.add(directionalLight);
 
   // 自然光
@@ -49,6 +57,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function render() {
     requestAnimationFrame(render);
+    handGltf.rotation.y += 0.001
     renderer.render(scene, camera);
   }
 });
