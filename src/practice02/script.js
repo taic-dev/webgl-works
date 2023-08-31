8103627af02124af12b618df9c72f25befd05bf8
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-08-31 09:36:57 +0900

    add hand vector

--- src/practice02/script.js
+++ src/practice02/script.js
@@ -7,7 +7,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 window.addEventListener("DOMContentLoaded", () => {
   // レンダー
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight - 100);
   renderer.setClearColor(new THREE.Color(0xffffff));
   const canvasElement = document.getElementById("webgl");
   canvasElement.appendChild(renderer.domElement);
@@ -20,14 +20,13 @@ window.addEventListener("DOMContentLoaded", () => {
   const gltfLoader = new GLTFLoader();
   gltfLoader.load(handGltfData, (gltf) => {
     handGltf = gltf.scene
-    handGltf.position.set(0, 0, 0)
-    handGltf.rotation.set(0, -4.5, -5)
+    handGltf.position.set(0, 0, 0)    
     scene.add(handGltf)
   })
-
+  
   // カメラ
   const camera = new THREE.PerspectiveCamera(
-    5,
+    4.5,
     window.innerWidth / window.innerHeight,
     1,
     1000
@@ -48,9 +47,10 @@ window.addEventListener("DOMContentLoaded", () => {
     const x = (clientX / innerWidth) * 2 -1
     const y = (clientY / innerHeight) * 2 -1
 
-    console.log(x, y)
-    handGltf.rotation.x += x * 0.001
-    handGltf.rotation.y += y * 0.001
+    // 手の向く方向ベクトルを変更してみる
+    const heightOnOrigin = (Math.tan(((15 * Math.PI / 180) / 2)) * 50 * 2)
+    const widthOnOrigin = heightOnOrigin * (window.innerWidth / window.innerHeight);
+    handGltf.lookAt(new THREE.Vector3(x * widthOnOrigin, y * heightOnOrigin, 50))
 
     camera.lookAt(new THREE.Vector3(x * 0.03, y * 0.03, 0,0));
   })
@@ -58,9 +58,10 @@ window.addEventListener("DOMContentLoaded", () => {
   // 画面のリサイズ
   window.addEventListener('resize', () => {
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth / window.innerHeight);
-    // camera.aspect = window.innerWidth / window.innerHeight;
+    renderer.setSize(window.innerWidth / window.innerHeight - 100);
+    camera.aspect = window.innerWidth / window.innerHeight - 100;
     camera.updateProjectionMatrix();
+    camera.lookAt(new THREE.Vector3(0.0, 0.0, 0,0));
   })
   
   // カメラ操作
@@ -78,16 +79,13 @@ window.addEventListener("DOMContentLoaded", () => {
   scene.add(ambientLight);
 
   // 軸ヘルパー
-  // const axesHelper = new THREE.AxesHelper(25);
-  // scene.add(axesHelper);
+  const axesHelper = new THREE.AxesHelper(25);
+  scene.add(axesHelper);
 
   render();
 
   function render() {
     requestAnimationFrame(render);
-
-    if(!handGltf) return
-    handGltf.rotation.y += 0.001
     renderer.render(scene, camera);
   }
 });
