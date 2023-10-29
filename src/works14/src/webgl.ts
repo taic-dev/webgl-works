a25cfd9e86fd4485a243fe7c067f0313b5557205
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-10-29 17:07:24 +0900

    add effect

--- src/works14/src/webgl.ts
+++ src/works14/src/webgl.ts
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import vertex from "../src/shader/vertex.glsl"
 import fragment from "../src/shader/fragment.glsl"
 
