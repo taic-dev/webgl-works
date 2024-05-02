bf44c3687cf7d1b5ccd1b05c87a0d1fc988bbe69
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-05-02 10:26:53 +0900

    work22 init

--- src/works22/src/ts/webgl.ts
+++ src/works22/src/ts/webgl.ts
@@ -1,9 +1,17 @@
+import * as THREE from "three";
+
 export class Webgl {
+  [x: string]: any;
   constructor() {
+    
+  }
+
+  _setRenderer() {
+    this.renderer = new THREE.WebGLRenderer()
 
   }
 
   init() {
-    
+
   }
 }
