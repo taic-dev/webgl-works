2b7cefab0929ad368d703870ea3c9e272869273a
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-06-03 23:42:12 +0900

    wip: scroll webgl

--- src/works24/src/ts/scroll.ts
+++ src/works24/src/ts/scroll.ts
@@ -7,13 +7,26 @@ const windowHeight = window.innerHeight;
 const listWrapper = document.querySelector(".list");
 const listWidth = listWrapper?.clientWidth;
 const listHeight = listWrapper?.clientHeight;
-const items = document.querySelectorAll(".item");
+
+const items = [...document.querySelectorAll(".item")];
 const rect = items[0].getBoundingClientRect();
 let { x, y } = clientRectCoordinate(rect);
 const topCoordinate = y;
 const bottomCoordinate = -y;
 const leftCoordinate = x;
 const rightCoordinate = -x;
+
+console.log(topCoordinate)
+
+let scrollNumArray = []
+let initCoordinate = []
+items.forEach((item) => {
+  scrollNumArray.push(0);
+  const rect = item.getBoundingClientRect();
+  initCoordinate.push(clientRectCoordinate(rect))
+});
+
+console.log(initCoordinate)
 
 let scrollNum = 0;
 
@@ -22,42 +35,49 @@ console.log(leftCoordinate, rightCoordinate);
 
 // const ticker = () => {
 window.addEventListener("wheel", (e: MouseEvent) => {
-  scrollNum += e.deltaY;
+  // scrollNum -= e.deltaY;
 
   items.forEach((item, i) => {
     const rect = item.getBoundingClientRect();
     let { x, y } = clientRectCoordinate(rect);
 
-    // scroll up
+    item.style.display = 'block';
+
+    scrollNumArray[i] -= e.deltaY;
+
+    // scroll up (minus)
     if (e.wheelDelta < 0) {
       gsap.to(item, {
-        y: scrollNum,
+        y: scrollNumArray[i],
         duration: 1,
         // ease: EASING.transform
       });
+    } 
+
+    console.log(y)
+
+    if(y > topCoordinate) {
+      console.log("action:",topCoordinate)
+      item.style.display = 'none';
+      item.style.transform = `translate(0, ${topCoordinate}px)`
+
+      scrollNumArray[i] = topCoordinate
+      // item.style.setProperty("transform", `translate(0, ${topCoordinate}px)`);
     }
 
     // scroll down
     if (e.wheelDelta > 0) {
       gsap.to(item, {
-        y: scrollNum,
+        y: scrollNumArray[i],
         duration: 1,
         // ease: EASING.transform
       });
     }
 
-    // if(y <= 0.0) {
-    //     scrollNum = 0
-    //     item.style.setProperty("transform", `translateY(${listHeight}px)`);
-    //   }
-
-    console.log(`[${i}]`, x, y);
-
-    if(y >= windowHeight) {
-      scrollNum = 0
-      item.style.setProperty("transform", `translateY(${topCoordinate}px)`);
-    }
+    // console.log(`[${i}]`, x, y);
   });
+
+  console.log(scrollNumArray)
 });
 // }
 
