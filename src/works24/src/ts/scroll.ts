d530780a4c682dc23a947a8888232dab84bbf16f
taic-dev <c.t.o.taishi.0530@gmail.com>
2024-06-05 20:39:19 +0900

    add: infinite scroll

--- src/works24/src/ts/scroll.ts
+++ src/works24/src/ts/scroll.ts
@@ -2,84 +2,40 @@ import gsap from "gsap";
 import { EASING } from "./constants";
 import { clientRectCoordinate } from "./utils";
 
-const windowWidth = window.innerWidth;
-const windowHeight = window.innerHeight;
-const listWrapper = document.querySelector(".list");
-const listWidth = listWrapper?.clientWidth;
-const listHeight = listWrapper?.clientHeight;
+const items = [...document.querySelectorAll<HTMLElement>(".item")];
+const itemHeight = items[0].clientHeight;
+const initPosition: number[] = []
+let scrollYArray: number[] = []
 
-const items = [...document.querySelectorAll(".item")];
-const rect = items[0].getBoundingClientRect();
-const { width } = rect;
-let { x, y } = clientRectCoordinate(rect);
-const topCoordinate = y;
-const bottomCoordinate = -y;
-const leftCoordinate = x;
-const rightCoordinate = -x;
-
-console.log(topCoordinate)
-
-let scrollNumArray = []
-let initCoordinate = []
-items.forEach((item) => {
-  scrollNumArray.push(0);
+items.forEach((item, i) => {
+  scrollYArray.push(0)
   const rect = item.getBoundingClientRect();
-  initCoordinate.push(clientRectCoordinate(rect))
-});
+  const { y } = clientRectCoordinate(rect);
+  initPosition.push(y);
+})
 
-console.log(initCoordinate)
-
-let scrollNum = 0;
-
-console.log(topCoordinate, bottomCoordinate);
-console.log(leftCoordinate, rightCoordinate);
-
-// const ticker = () => {
-window.addEventListener("wheel", (e: MouseEvent) => {
-  // scrollNum -= e.deltaY;
-
+window.addEventListener("wheel", (e: WheelEvent) => {
   items.forEach((item, i) => {
     const rect = item.getBoundingClientRect();
-    let { x, y } = clientRectCoordinate(rect);
+    const { y } = clientRectCoordinate(rect);
 
-    item.style.display = 'block';
+    scrollYArray[i] -= e.deltaY;
 
-    scrollNumArray[i] -= e.deltaY;
+    // slide up
+    if(y > initPosition[0] + initPosition[0] / 4) {
 
-    // scroll up (minus)
-    if (e.wheelDelta < 0) {
-      gsap.to(item, {
-        y: scrollNumArray[i],
-        duration: 1,
-        // ease: EASING.transform
-      });
-    } 
+      item.style.transform = `translate(0, ${itemHeight * 5 + 15 * 5 + scrollYArray[i]}px)`;
+      scrollYArray[i] = itemHeight * 5 + 15 * 5 + scrollYArray[i]
+    }
+    
+    // slide down
+    if(y < initPosition[items.length - 1] + initPosition[items.length - 1] / 4) {
 
-    console.log(y)
-
-    if(y > topCoordinate) {
-      console.log("action:",topCoordinate)
-      item.style.display = 'none';
-      item.style.transform = `translate(0, ${listHeight - width - 45}px)`
-
-      scrollNumArray[i] = listHeight
-      // item.style.setProperty("transform", `translate(0, ${topCoordinate}px)`);
+      item.style.transform = `translate(0, -${itemHeight * 5 - 15 * 5 + scrollYArray[i]}px)`;
+      scrollYArray[i] = -itemHeight * 5 - 15 * 5 + scrollYArray[i]
     }
 
-    // scroll down
-    if (e.wheelDelta > 0) {
-      gsap.to(item, {
-        y: scrollNumArray[i],
-        duration: 1,
-        // ease: EASING.transform
-      });
-    }
+    item.style.transform = `translate(0, ${scrollYArray[i]}px)`;
 
-    // console.log(`[${i}]`, x, y);
   });
-
-  console.log(scrollNumArray)
 });
-// }
-
-// requestAnimationFrame(ticker)
