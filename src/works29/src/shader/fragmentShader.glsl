d643a6692692a75e7977ce735fd9393837cccc36
taic-dev <c.t.o.taishi.0530@gmail.com>
2025-01-04 17:14:45 +0900

    fix

--- src/works29/src/shader/fragmentShader.glsl
+++ src/works29/src/shader/fragmentShader.glsl
@@ -3,12 +3,5 @@ varying vec2 vUv;
 
 void main() {
   vec4 color = texture2D(uTexture, vUv);
-
-  // if(color.r > 0. && color.g > 0. && color.b > 0.){
-  //   discard;
-  // };
-
-  // color.a = 0.3;
-
   gl_FragColor = color;
 }
